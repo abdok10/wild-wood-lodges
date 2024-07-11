88468fb0a191337e875e8a73e8dfb645d52309e0
karchaoui Abdessamad <140338304+abdok10@users.noreply.github.com>
2024-07-12 00:27:08 +0100

    finished basics config and routes set up

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "About",
+};
+
+export default async function Page() {
+  return (
+    <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
+      <div className="col-span-3">
+        <h1 className="text-4xl mb-10 text-accent-400 font-medium">
+          Welcome to The Wild Oasis
+        </h1>
+      </div>
+    </div>
+  );
+}
