c209ae666bf669dcbc74350d0a2650b1bc68889c
karchaoui Abdessamad <140338304+abdok10@users.noreply.github.com>
2024-07-22 20:43:26 +0100

    routes protected, signIn & signOut customized

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,11 +1,15 @@
+import { auth } from "@lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
 export default async function Account() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ")[0];
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Abdo 🎉
+      Welcome, {firstName} 🎉
     </h2>
   );
 }
